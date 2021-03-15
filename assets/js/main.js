f570b04bd69778865506c334fdaa49d162e19141
Andrea Marangon <marangonandrea84@gmail.com>
2021-03-15 19:07:05 +0100

    ESERCIZIO TERMINATO

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,36 +1,39 @@
 $(document).ready(function() {
-  randomNumberInRange(1,100);
+
+  //creo un array vuoto per ospitare i numeri casuali
+  var randomNumbers = [];
+
+  //creo un array vuoto per ospitare i numeri indovinati dall'utente
+  var userNumbers = [];
+
+  //genero 5 numeri casuali e li inserisco nell'array randomNumbers
   createNumber(randomNumbers);
 
-  //genero 5 numeri casuali e li inserisco nell'array randomNumber
-  $('#random-numbers').text(randomNumbers);
+  //stampo i numeri random nel DOM
+  $('#random-numbers').text('Memorizza i seguenti numeri: ' + randomNumbers);
+  console.log(randomNumbers);
 
   //faccio partire un timer di 30 secondi e nascondo i numeri
   setTimeout(function () {
       $('#random-numbers').hide();
-    },3000);
+    },30000);
 
-  //chiedo all'utente di inserire per 5 volte un numero che ha visto precedentemente
-  for (var i = 0; i < 5; i++) {
-    var userNumber = parseInt(prompt('Inserisci un numero'));
-    userNumbers.push(userNumber);
-  }
-  console.log(userNumbers);
-  //con un prompt
-  //il software dice quanti e quali numeri sono stati individuati
-
+  //chiedo all'utente di inserire per 5 volte un numero che ha visto precedentemente con un prompt
+  setTimeout(function () {
+    for (var i = 0; i < 5; i++) {
+      var userNumber = parseInt(prompt('Inserisci un numero'));
+      //metto i numeri indovinati in un array randomNumbers
+      if (randomNumbers.includes(userNumber)) {
+        userNumbers.push(userNumber);
+      }
+    }
+    console.log(userNumber);
+    console.log(userNumbers);
+    //il software dice quanti e quali numeri sono stati individuati
+    $('#exact-numbers').text('Hai indovinato i seguenti numeri: ' + userNumbers);
+    $('#quantity-exact-numbers').text('Hai indovinato ' + userNumbers.length + ' numeri');
+  },31000);
 });
-//creo un array vuoto per ospitare i numeri casuali
-var randomNumbers = [];
-//creo un array vuoto per ospitare i numeri dati da utente
-var userNumbers = [];
-// randomNumbers = createNumber(array);
-console.log(randomNumbers);
-
-
-
-
-
 
 //FUNCTIONS
 //funzione per creare 5 numeri casuali
